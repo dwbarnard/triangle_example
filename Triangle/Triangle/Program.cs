dbd84f709f05144f9e4cf7afe0084f981368d9e8
dwbarnard <35636703+dwbarnard@users.noreply.github.com>
2018-01-30 20:29:19 -0500

    Initial commit

---
+++ Triangle/Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿// Dave B.
+
+using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+			for (int triangleIndex = 1; triangleIndex < 10; triangleIndex++)	// will be the char displayed in the triangles
+			{
+				for (int digitIndex = 1; digitIndex <= triangleIndex; digitIndex++)	// how many lines per triangle
+				{
+					for (int lineIndex = 1; lineIndex <= digitIndex; lineIndex++)	// how many chars on each line
+					{
+						Console.Write(triangleIndex);
+					}
+					Console.WriteLine();
+				}
+				Console.WriteLine();
+			}
+
+			// keep console open until a key is pressed
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					break;	// out of while loop
+				}
+			}
+        }
+    }
+}
